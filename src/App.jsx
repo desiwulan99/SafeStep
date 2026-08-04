5b44b81fd9a6c25ff4ccc7bb2497dfe54e0ba512
desiwulan99 <desiwulansari883@gmail.com>
2026-08-04 15:46:13 +0700

    create guardian feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import HomePage from "./pages/HomePage";
 import ReportPage from "./pages/ReportPage";
 import SafeRoutePage from "./pages/SafeRoutePage";
+import LiveGuardianPage from "./pages/LiveGuardianPage";
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState("home");
@@ -12,6 +13,8 @@ export default function App() {
       setCurrentPage("report");
     } else if (key === "safe-route") {
       setCurrentPage("safe-route");
+    } else if (key === "live-guardian") {
+      setCurrentPage("live-guardian");
     } else {
       setCurrentPage("home");
     }
@@ -23,6 +26,10 @@ export default function App() {
 
   if (currentPage === "safe-route") {
     return <SafeRoutePage userName="user" onNavigate={handleNavigate} />;
+  }
+
+  if (currentPage === "live-guardian") {
+    return <LiveGuardianPage userName="user" onNavigate={handleNavigate} />;
   }
 
   return <HomePage userName="user" onNavigate={handleNavigate} />;
