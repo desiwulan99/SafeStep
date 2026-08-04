2c9ef11da94ffa237e6e19b1aadbd1b88984de8e
desiwulan99 <desiwulansari883@gmail.com>
2026-08-04 13:38:04 +0700

    uprgade safe-route feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
 import HomePage from "./pages/HomePage";
 import ReportPage from "./pages/ReportPage";
+import SafeRoutePage from "./pages/SafeRoutePage";
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState("home");
 
   const handleNavigate = (item) => {
-    if (item?.key === "report") {
+    const key = item?.key || item;
+    if (key === "report") {
       setCurrentPage("report");
-    } else if (item?.key === "home") {
+    } else if (key === "safe-route") {
+      setCurrentPage("safe-route");
+    } else {
       setCurrentPage("home");
     }
   };
 
   if (currentPage === "report") {
     return <ReportPage userName="user" onNavigate={handleNavigate} />;
+  }
+
+  if (currentPage === "safe-route") {
+    return <SafeRoutePage userName="user" onNavigate={handleNavigate} />;
   }
 
   return <HomePage userName="user" onNavigate={handleNavigate} />;
